c0d98d62d528f75ed88de1c333ea3e48194d5393
Amar Bains <amarjit.bains@gmail.com>
2013-02-27 15:06:40 -0500

    respond

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Intelligentstaffing::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.assets.precompile += %w( modernizr.js respond.js respond-proxy.html respond.proxy.js )
 end
